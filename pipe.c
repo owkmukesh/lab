4408468caa931ed31fa3e7f0bb1b8d34dd97e924
owkmukesh <owku.mukesh@gmail.com>
2025-04-24 08:47:42 +0530

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
+
 #include <unistd.h>
+
 #include <sys/types.h>
+
 #include <sys/wait.h>
 
+
+
 int main() {
-    int fd[2];
+
+    int fd[2], n;
+
     char buffer[100];
+
     pid_t p;
 
-    if (pipe(fd) == -1) {
-        perror("pipe failed");
-        return 1;
-    }
+
+
+    pipe(fd);
 
     p = fork();
 
-    if (p > 0) {
-        // Parent process
-        close(fd[0]); // Close reading end
-        printf("Parent passing value to child\n");
-        write(fd[1], "hello", 6); // Write to pipe
-        close(fd[1]); // Close writing end
-        wait(NULL); // Wait for child process
-    } else if (p == 0) {
-        // Child process
-        close(fd[1]); // Close writing end
-        int n = read(fd[0], buffer, sizeof(buffer));
-        buffer[n] = '\0'; // Null-terminate the string
-        printf("%s", buffer);
-        close(fd[0]); // Close reading end
+
+
+    if(p > 0) {
+
+        printf("Passing value to child\n");
+
+        write(fd[1], "hello\n", 6);
+
     } else {
-        // Fork failed
-        perror("Fork failed");
-        return 1;
+
+        printf("Child received data\n");
+
+        n = read(fd[0], buffer, 100);
+
+        write(1, buffer, n);
+
     }
 
+
+
     return 0;
+
 }
